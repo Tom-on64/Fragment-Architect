a4715dfdc42f29e029e0699b693279e3fd95f913
Tom-on64 <kubecekt@gmail.com>
2022-06-26 16:23:14 +0200

    Added movement, a world and player
    Added:
        - Movement (Left, Right, Jump, Gravity)
        - Player, Shows a red line where the player is looking
        - World, For now just a red plane (idk how to randomly generate and how to add propper collision)

--- js/index.js
+++ js/index.js
@@ -6,6 +6,83 @@ canvas.width = canvasBoundingBox.width
 canvas.height = canvasBoundingBox.height
 
 // Variables
+downPressed = false
+upPressed = false
+leftPressed = false
+rightPressed = false
+
+
+// Functions
+document.addEventListener("keydown", keyDown)
+document.addEventListener("keyup", keyUp)
+
+function keyDown(event) {
+    if (event.keyCode == 40) {
+        player.dir = "up"
+        downPressed = true
+    }
+    if (event.keyCode == 38) {
+        player.dir = "down"
+        upPressed = true
+    }
+    if (event.keyCode == 37) {
+        player.dir = "left"
+        leftPressed = true
+    }
+    if (event.keyCode == 39) {
+        player.dir = "right"
+        rightPressed = true
+    }
+}
+function keyUp(event) {
+    if (event.keyCode == 40) {
+        downPressed = false
+    }
+    if (event.keyCode == 38) {
+        upPressed = false
+    }
+    if (event.keyCode == 37) {
+        leftPressed = false
+    }
+    if (event.keyCode == 39) {
+        rightPressed = false
+    }
+}
+
+function inputs() {
+    if (upPressed == true & player.y >= world.ground - player.radius) {
+        player.y -= player.speed * 20
+    }
+    if (leftPressed == true) {
+        player.x -= player.speed
+    }
+    if (rightPressed == true) {
+        player.x += player.speed
+    }
+}
+
+function boundryCheck () {
+    // Up
+    if (player.y < player.radius) {
+        player.y = player.radius
+    }
+    // Down
+    if (player.y > world.ground - player.radius) {
+        player.y = world.ground - player.radius
+    }
+    // Left
+    if (player.x < player.radius) {
+        player.x = player.radius
+    }
+    // Right
+    if (player.x > canvas.width - player.radius) {
+        player.x = canvas.width - player.radius
+    }
+}
+
+function gravity() {
+    player.y += player.y / 60
+}
 
 // Player
 class Player {
@@ -13,6 +90,8 @@ class Player {
         this.x = canvas.width / 2
         this.y = canvas.height / 2
         this.radius = 50
+        this.speed = 10
+        this.dir = "right"
     }
     draw() {
         ctx.fillStyle = 'lime'
@@ -21,12 +100,38 @@ class Player {
         ctx.fill()
         ctx.closePath()
         ctx.fill()
-        ctx.strokeStyle = 'red'
-        ctx.lineWidth = 1.5
-        ctx.beginPath()
-        ctx.moveTo(this.x, this.y)
-        ctx.lineTo(this.x + 100, this.y)
-        ctx.stroke()
+        if (this.dir == "right") {
+            ctx.strokeStyle = 'red'
+            ctx.lineWidth = 1.5
+            ctx.beginPath()
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x + 100, this.y)
+            ctx.stroke()
+        }
+        else if (this.dir == "left") {
+            ctx.strokeStyle = 'red'
+            ctx.lineWidth = 1.5
+            ctx.beginPath()
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x - 100, this.y)
+            ctx.stroke()
+        }
+        else if (this.dir == "up") {
+            ctx.strokeStyle = 'red'
+            ctx.lineWidth = 1.5
+            ctx.beginPath()
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x, this.y + 100)
+            ctx.stroke()
+        }
+        else if (this.dir == "down") {
+            ctx.strokeStyle = 'red'
+            ctx.lineWidth = 1.5
+            ctx.beginPath()
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x, this.y - 100)
+            ctx.stroke()
+        }
     }
 }
 const player = new Player()
@@ -34,16 +139,13 @@ const player = new Player()
 // World
 class World {
     constructor() {
-        this.x = canvas.width / 2
-        this.y = canvas.height / 1
+        this.x = 0
+        this.y = canvas.height / 1.20
+        this.ground = this.y
     }
     draw() {
         ctx.fillStyle = "red"
-        ctx.beginPath()
-        ctx.fillRect(this.x - 200, this.y -200, this.x + 200, this.y + 200)
-        ctx.fill()
-        ctx.closePath()
-        ctx.fill()
+        ctx.fillRect(this.x, this.y, canvas.width, canvas.height)
     }
 }
 const world = new World()
@@ -52,11 +154,14 @@ function render() {
     canvasBoundingBox = canvas.getBoundingClientRect()
     canvas.width = canvasBoundingBox.width
     canvas.height = canvasBoundingBox.height
+    inputs()
+    gravity()
+    boundryCheck()
     ctx.fillStyle = "lightblue"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     world.draw()
     player.draw()
-    // requestAnimationFrame(render)
+    requestAnimationFrame(render)
 }
 
 render()
